d0e814eed6de38aed00cce56ba204200fa6d0ae0
Maksim Zablin <mzabelin8@mail.ru>
2023-01-23 18:09:05 +0300

    add typedef func

--- tools/typedef.py
+++ tools/typedef.py
@@ -1,1 +1,9 @@
-print("hey")
+import pandas as pd
+
+
+def typedef(data: pd.DataFrame) -> dict:
+    res = {}
+    if type(data) == pd.core.frame.DataFrame:
+        for col in data.columns:
+            res[col] = str(data[col].dtypes)
+    return res
